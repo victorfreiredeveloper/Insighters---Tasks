473c8a4abee6e4fe1f8158ea03de2d1b40428439
Victor Hugo Freire Pinheiro Pereira <victor_freire@outlook.com>
2021-11-20 11:38:07 -0300

    .

--- Task-Complete.js
+++ Task-Complete.js
@@ -4,12 +4,12 @@ const database = {
         const regexp           = /create table ([a-z]+) \((.+)\)/;
         const parsedStatement  = commandDDL.match(regexp);
         const tableName        = parsedStatement[1];
-        let   columns     = parsedStatement[2];
-        columns           = columns.split(", ");
+        let   columns          = parsedStatement[2];
+        columns                = columns.split(", ");
         for (let column of columns) {
-            column = column.split(' ');
-            const name = column[0];
-            const type = column[1];
+            column        = column.split(' ');
+            const name    = column[0];
+            const type    = column[1];
             columns[name] = type;
         }
         this.tables[tableName] = {columns, data : []};
